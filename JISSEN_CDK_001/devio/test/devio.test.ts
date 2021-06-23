2b71f3a1bf695143b74e29129d74a12045e86147
tkoba0330 <t.koba0330@gmail.com>
2021-06-23 21:07:33 +0900

    じっせん！ 003 テスト完了

--- JISSEN_CDK_001/devio/test/devio.test.ts
+++ JISSEN_CDK_001/devio/test/devio.test.ts
@@ -1,13 +1,16 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect , countResources, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Devio from '../lib/devio-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Devio.DevioStack(app, 'MyTestStack');
+    const stack = new Devio.DevioStack(app, 'DevioStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(stack).to(countResources('AWS::EC2::VPC', 1));
+    expect(stack).to(haveResource('AWS::EC2::VPC', {
+      CidrBlock: '10.0.0.0/16',
+      Tags: [{ 'Key': 'Name', 'Value': 'devio-stg-vpc'}]
+    }));
+
 });

4c802b205541ddbbfc82529576421a8bfc444a0f
tkoba0330 <t.koba0330@gmail.com>
2021-06-23 23:14:57 +0900

    じっせん! 004 subnet

--- JISSEN_CDK_001/devio/lib/devio-stack.ts
+++ JISSEN_CDK_001/devio/lib/devio-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core';
-import { CfnVPC } from '@aws-cdk/aws-ec2';
+import { CfnSubnet, CfnVPC, Vpc } from '@aws-cdk/aws-ec2';
 
 export class DevioStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,9 +9,27 @@ export class DevioStack extends cdk.Stack {
     const envType = this.node.tryGetContext('envType');
 
     // The code that defines your stack goes here
-    new CfnVPC(this, 'Vpc', {
+    const vpc = new CfnVPC(this, 'Vpc', {
       cidrBlock: '10.0.0.0/16',
       tags: [{ key: 'Name', value: `${systemName}-${envType}-vpc` }]
+    });
+
+    const subnetparams = [
+      {cidr: '11', az: '1a', subname: 'public'},
+      {cidr: '12', az: '1c', subname: 'public'},
+      {cidr: '21', az: '1a', subname: 'app'},
+      {cidr: '22', az: '1c', subname: 'app'},
+      {cidr: '31', az: '1a', subname: 'db'},
+      {cidr: '32', az: '1c', subname: 'db'},
+    ]
+
+    subnetparams.forEach(e => {
+      new CfnSubnet(this, `Subnet${e.subname.substring(0, 1).toUpperCase() + e.subname.substring(1)}${e.az}`, {
+        cidrBlock: `10.0.${e.cidr}.0/24`,
+        vpcId: vpc.ref,
+        availabilityZone: `ap-northeast-${e.az}`,
+        tags: [{key: 'Name', value: `${systemName}-${envType}-subnet-${e.subname}-${e.az}`}]
+      });
     });
   }
 }
--- JISSEN_CDK_001/devio/test/devio.test.ts
+++ JISSEN_CDK_001/devio/test/devio.test.ts
@@ -18,4 +18,20 @@ test('Empty Stack', () => {
       Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-vpc'}]
     }));
 
+    const subnetparams = [
+      {cidr: '11', az: '1a', subname: 'public'},
+      {cidr: '12', az: '1c', subname: 'public'},
+      {cidr: '21', az: '1a', subname: 'app'},
+      {cidr: '22', az: '1c', subname: 'app'},
+      {cidr: '31', az: '1a', subname: 'db'},
+      {cidr: '32', az: '1c', subname: 'db'},
+    ]
+
+    expect(stack).to(countResources('AWS::EC2::Subnet', 1));
+    expect(stack).to(haveResource('AWS::EC2::Subnet', {
+      CidrBlock: '10.0.11.0/24',
+      AvailabilityZone: 'ap-northeast-1a',
+      Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-subnet-public-1a' }]
+    }));
+    
 });

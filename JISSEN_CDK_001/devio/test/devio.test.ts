ffcaca76be4456ed0e699639d33144a7f9f3bcc3
tkoba0330 <t.koba0330@gmail.com>
2021-06-23 21:30:51 +0900

    じっせん！004 完了

--- JISSEN_CDK_001/devio/test/devio.test.ts
+++ JISSEN_CDK_001/devio/test/devio.test.ts
@@ -3,14 +3,19 @@ import * as cdk from '@aws-cdk/core';
 import * as Devio from '../lib/devio-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
+    const app = new cdk.App({
+      context: {
+        'systemName': 'starwars',
+        'envType': 'prd'
+      }
+    });
     // WHEN
     const stack = new Devio.DevioStack(app, 'DevioStack');
     // THEN
     expect(stack).to(countResources('AWS::EC2::VPC', 1));
     expect(stack).to(haveResource('AWS::EC2::VPC', {
       CidrBlock: '10.0.0.0/16',
-      Tags: [{ 'Key': 'Name', 'Value': 'devio-stg-vpc'}]
+      Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-vpc'}]
     }));
 
 });

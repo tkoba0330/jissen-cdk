21162b3e9fdd48f0b844ff41719721621e3da8d1
tkoba0330 <t.koba0330@gmail.com>
2021-06-23 20:44:04 +0900

    じっせん！002 VPN

--- JISSEN_CDK_001/devio/lib/devio-stack.ts
+++ JISSEN_CDK_001/devio/lib/devio-stack.ts
@@ -1,11 +1,14 @@
-import { Vpc } from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
+import { CfnVPC } from '@aws-cdk/aws-ec2';
 
 export class DevioStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    new Vpc(this, 'Vpc');
+    new CfnVPC(this, 'Vpc', {
+      cidrBlock: '10.0.0.0/16',
+      tags: [{ key: 'Name', value: 'devio-stg-vpc' }]
+    });
   }
 }
